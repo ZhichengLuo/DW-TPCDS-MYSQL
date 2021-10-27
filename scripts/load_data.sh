0be2d5de71fb303a5e6539bf0004254cef815cf0
zhichengluo <zcsysu@163.com>
2021-10-27 21:29:14 +0800

    --story=mysqlimport with named pipes doesn't wrok

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -15,30 +15,25 @@ cd $DATA_DIR
 
 count=1
 total=`ls *.dat | wc -l`
-echo "start loading data from $total files..."
 
+# Diable FOREIGN_KEY_CHECKS to speed up the data loading. After loading is done, it will be set back.
 sudo mysql -uroot -e "SET global FOREIGN_KEY_CHECKS=0;"
-sudo mysql -uroot -e "SET global autocommit=0;"
 
-ls *.pipe* | xargs rm -r
-echo "generating pipe file..."
 ls *.dat | while read file; do
     table=`basename $file .dat | sed -e 's/_[0-9]_[0-9]//'`
-    pipe=$table.pipe$count 
+    echo "($count/$total) `date +"%D %T"` `ls -sh $file` to $table"
+
+    # Utilize named pipe to avoid unnecessary temporary files on disk
+    pipe=$table.pipe
     rm -f $pipe
     mkfifo $pipe
-    LANG=C && sed -e 's_^|_\\N|_g' -e 's_||_|\\N|_g' -e 's_||_|\\N|_g' $file > $pipe &
+    # Replace empty string to '\N' as MySQL doesn't convert empty string into NULL values 
+    LANG=C && sed -e 's_^|_\\N|_g' -e 's_||_|\\N|_g' -e 's_||_|\\N|_g' $file > $pipe & \
+    mysql --local-infile -D$DATABASE -e \
+        "load data local infile '$pipe' replace into table $table character set latin1 fields terminated by '|'"
+    
+    rm -f $pipe
     let count++
 done
 
-echo "loading using mysqlimport..."
-ls *.pipe* | xargs mysqlimport --local $DATABASE \
-                --use-threads=8 \
-                --default-character-set=latin1 \
-                --fields-terminated-by='|' \
-                --replace \
-                --silent 
-
-
 sudo mysql -uroot -e "SET global FOREIGN_KEY_CHECKS=1;"
-sudo mysql -uroot -e "SET global autocommit=1;"
